0af87f0ef94f9bf3aeaf77f420d96816322a2d3b
Anjali2201 <anjalikushwaha031@gmail.com>
2023-03-07 02:23:07 +0530

    cards section

--- src/Components/Feed/card.jsx
+++ src/Components/Feed/card.jsx
@@ -1,55 +1,133 @@
 import React from "react";
-import {
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Button,
-  Typography,
-} from "@mui/material";
 
-const Boxes = () => {
-  return (
-    <div>
-      <Card
-        sx={{
-          maxWidth: 350,
-          p: 2,
-          display: "flex",
-          flexDirection: "column",
-          flexWrap: "wrap",
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "#212225",
-          borderRadius: "10px",
-          boxShadow: "0px 0px 10px 0px #000000",
-          "&:hover": {
-            boxShadow: "0px 0px 10px 0px #ffffff",
-            cursor: "pointer",
-          },
-        }}
-      >
-        <CardMedia
-          component="img"
-          height="140"
-          image="https://images.pexels.com/photos/4523062/pexels-photo-4523062.jpeg?auto=compress&cs=tinysrgb&w=600"
-          alt="random"
-        />
-        <CardContent>
-          <Typography sx={{ color: "white" }}>
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with your guests. Add 1 cup of frozen peas along with
-            the mussels, if you like.
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">
-            <Typography sx={{ color: "white" }}>Share</Typography>
-          </Button>
-        </CardActions>
-      </Card>
-    </div>
-  );
+const card = () => {
+  return <div>card</div>;
 };
 
-export default Boxes;
+export default card;
+
+// <Card
+// sx={{
+//   width: "400px",
+//   minHeight: 300,
+//   p: 3,
+//   display: "flex",
+//   flexDirection: "column",
+//   flexWrap: "wrap",
+//   alignItems: "center",
+//   borderRadius: "25px",
+//   // backgroundColor: "#EEF0F2",
+// }}
+// >
+// <CardMedia
+//   component="img"
+//   height="140"
+//   image="https://th.bing.com/th/id/OIP.CIjnLNfuNOZ_71H2g1nE5gHaF7?pid=ImgDet&rs=1"
+//   alt="random"
+// />
+// <CardHeader title={post.gadgetname} />
+// {/* <Typography sx={{ color: "black" }}>{post.category}</Typography> */}
+// {/* <Typography sx={{ color: "black" }}>{post.model}</Typography>
+// <Typography sx={{ color: "black" }}>{post.price}</Typography>
+// <Typography sx={{ color: "black" }}>{post.rate}</Typography>
+// <Typography sx={{ color: "black" }}>{post.authoremail}</Typography> */}
+
+// <CardContent>
+//   <Grid
+//     item
+//     xs={12}
+//     sx={{
+//       display: "flex",
+//       flexDirection: "row",
+//       justifyContent: "space-between",
+//     }}
+//   >
+//     <Typography
+//       sx={{
+//         color: "black",
+//         fontSize: "15px",
+//         fontWeight: "bold",
+//         backgroundColor: "#83c5be",
+//         borderRadius: "10px",
+//         padding: "5px 10px",
+//       }}
+//     >
+//       {post.category}
+//     </Typography>
+//     <Typography
+//       sx={{
+//         color: "black",
+//         fontSize: "15px",
+//         fontWeight: "bold",
+//         backgroundColor: "#83c5be",
+//         borderRadius: "10px",
+//         padding: "5px 10px",
+//       }}
+//     >
+//       {post.model}
+//     </Typography>
+//   </Grid>
+//   <Grid
+//     item
+//     xs={12}
+//     sx={{
+//       my: "15px",
+//     }}
+//   >
+//     <Typography
+//       sx={{
+//         color: "black",
+//         fontSize: "16px",
+//         backgroundColor: "#EEF0F2",
+//         padding: "5px 10px",
+//       }}
+//     >
+//       {post.review.slice(0, 100)} ...{" "}
+//       <Button onClick={handleOpen}>Read More</Button>
+//     </Typography>
+//     <Typography
+//       sx={{
+//         color: "black",
+//         fontSize: "15px",
+//         textAlign: "right",
+//         mt: "10px",
+//       }}
+//     >
+//       By{" "}
+//       <span style={{ fontWeight: "light" }}>
+//         {" "}
+//         {post.authorname}
+//       </span>
+//     </Typography>
+//   </Grid>
+// </CardContent>
+
+// <CardActions sx={{ mt: "10px", p: 0 }}>
+//   <Grid
+//     item
+//     sx={{
+//       display: "flex",
+//       flexDirection: "row",
+//       justifyContent: "center",
+//     }}
+//   >
+//     <Button
+//       variant="contained"
+//       sx={{
+//         backgroundColor: "transparent",
+//         mr: "10px",
+//       }}
+//     >
+//       <ThumbUpIcon sx={{ color: "black", fontSize: "25px" }} />
+//     </Button>
+//     <Button
+//       variant="contained"
+//       sx={{
+//         backgroundColor: "transparent",
+//       }}
+//     >
+//       <ChatBubbleIcon sx={{ color: "black", fontSize: "25px" }} />
+//     </Button>
+//   </Grid>
+// </CardActions>
+// </Card>
--- src/Components/Feed/feed.jsx
+++ src/Components/Feed/feed.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Button, CardActions, CardHeader, Grid } from "@mui/material";
+import {
+  Button,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  Grid,
+} from "@mui/material";
 import { Typography } from "@mui/material";
 import { Link } from "@mui/material";
 import { Card, CardContent } from "@mui/material";
@@ -7,6 +13,20 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Stack } from "@mui/system";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const Feedpage = () => {
   const [post, setMyData] = useState([]);
@@ -31,7 +51,9 @@ const Feedpage = () => {
   // "rate": "3",
   // "_id": "63e40ee38ae613ad2518738e",
   // "__v": 0
-
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <div>
       <Grid
@@ -40,7 +62,7 @@ const Feedpage = () => {
           justifyContent: "center",
           px: 3,
           pb: 4,
-          backgroundColor: "#FAFAFF",
+          backgroundColor: "#EEF0F2",
           minHeight: "100vh",
           height: "auto",
         }}
@@ -59,9 +81,7 @@ const Feedpage = () => {
             Reviews
           </Typography>
         </Grid>
-
         {/* ------------------------------Chips---------------------------------- */}
-
         {/* <Grid
           item
           xs={12}
@@ -72,7 +92,6 @@ const Feedpage = () => {
         >
           <ChipsArray />
         </Grid> */}
-
         {/* ------------------------------Left Bar---------------------------------- */}
         <Grid
           item
@@ -102,7 +121,6 @@ const Feedpage = () => {
             </Button>
           </Link>
         </Grid>
-
         {/* ------------------------------Post---------------------------------- */}
         <Grid
           item
@@ -121,67 +139,142 @@ const Feedpage = () => {
           {post.map((post) => (
             <Card
               sx={{
-                minWidth: 350,
-                maxWidth: 350,
-                minHeight: 500,
-                maxHeight: 500,
-                p: 2,
+                width: "400px",
+                height: "auto",
+                p: 3,
                 display: "flex",
                 flexDirection: "column",
                 flexWrap: "wrap",
                 alignItems: "center",
-                borderRadius: "10px",
+                borderRadius: "25px",
+                // backgroundColor: "#EEF0F2",
               }}
             >
-              <CardHeader title={post.gadgetname} sx={{ color: "black" }} />
-              {/* <Typography sx={{ color: "black" }}>{post.category}</Typography> */}
-
-              {/* <Typography sx={{ color: "black" }}>{post.model}</Typography>
-              <Typography sx={{ color: "black" }}>{post.price}</Typography>
-              <Typography sx={{ color: "black" }}>{post.rate}</Typography>
-             
-              <Typography sx={{ color: "black" }}>{post.authoremail}</Typography> */}
+              <CardMedia
+                component="img"
+                height="140"
+                image="https://th.bing.com/th/id/OIP.CIjnLNfuNOZ_71H2g1nE5gHaF7?pid=ImgDet&rs=1"
+                alt="random"
+              />
+              <CardHeader title={post.gadgetname} />
 
               <CardContent>
-                <Typography sx={{ color: "black" }}>
-                  {post.review.slice(0, 100)} ...{" "}
-                  <Link href="/review">Read More</Link>
-                </Typography>
-              </CardContent>
-              <Typography
-                sx={{
-                  color: "black",
-                  fontSize: "15px",
-                }}
-              >
-                By{" "}
-                <span style={{ fontWeight: "light" }}> {post.authorname}</span>
-              </Typography>
-
-              <CardActions sx={{ mt: "auto" }}>
-                <Stack direction="row" spacing={2}>
-                  <Button
-                    variant="contained"
+                <Grid
+                  item
+                  xs={12}
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Typography
                     sx={{
+                      color: "black",
+                      fontSize: "15px",
+                      fontWeight: "bold",
                       backgroundColor: "#83c5be",
+                      borderRadius: "10px",
+                      padding: "5px 10px",
                     }}
                   >
-                    <ThumbUpIcon sx={{ color: "black" }} />
+                    {post.category}
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: "black",
+                      fontSize: "15px",
+                      fontWeight: "bold",
+                      backgroundColor: "#83c5be",
+                      borderRadius: "10px",
+                      padding: "5px 10px",
+                    }}
+                  >
+                    {post.model}
+                  </Typography>
+                </Grid>
+                <Grid
+                  item
+                  xs={12}
+                  sx={{
+                    my: "15px",
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      color: "black",
+                      fontSize: "16px",
+                      backgroundColor: "#EEF0F2",
+                      padding: "5px 10px",
+                    }}
+                  >
+                    {post.review.slice(0, 100)} ...{" "}
+                    <Button onClick={handleOpen}>Read More</Button>
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: "black",
+                      fontSize: "15px",
+                      textAlign: "right",
+                      mt: "10px",
+                    }}
+                  >
+                    By{" "}
+                    <span style={{ fontWeight: "light" }}>
+                      {" "}
+                      {post.authorname}
+                    </span>
+                  </Typography>
+                </Grid>
+              </CardContent>
+
+              <CardActions sx={{ mt: "10px", p: 0 }}>
+                <Grid
+                  item
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Button
+                    variant="contained"
+                    sx={{
+                      backgroundColor: "transparent",
+                      mr: "10px",
+                    }}
+                  >
+                    <ThumbUpIcon sx={{ color: "black", fontSize: "25px" }} />
                   </Button>
                   <Button
                     variant="contained"
                     sx={{
-                      border: "1px solid black",
-                      backgroundColor: "#DADDD8",
+                      backgroundColor: "transparent",
                     }}
                   >
-                    <Typography sx={{ color: "black" }}>Comment</Typography>
+                    <ChatBubbleIcon sx={{ color: "black", fontSize: "25px" }} />
                   </Button>
-                </Stack>
+                </Grid>
               </CardActions>
             </Card>
           ))}
         </Grid>
+        {/* -----------------------------Modal---------------------------------- */}
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Modal>
       </Grid>
     </div>
   );
